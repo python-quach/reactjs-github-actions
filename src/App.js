6e02a922cdf288dc3663b0c2d8a6344aa3510e8c
Python Development <python@Hungs-MacBook-Pro.local>
2020-10-17 16:21:12 -0700

    remove stuff from app.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return (
-        <div className='App'>
-            <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
-                <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-                    Learn React With Actions on Github
-                </a>
-            </header>
-        </div>
-    );
+    return <div className='App'></div>;
 }
 
 export default App;
